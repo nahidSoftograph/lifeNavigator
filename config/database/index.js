18b6fdbaf15bf940e92758eb9d4fa71692df542b
bmshamsnahid <b.m.shamsnahid@gmail.com>
2018-07-05 12:27:36 +0600

    added sequential category id

--- config/database/index.js
+++ config/database/index.js
@@ -1,4 +1,4 @@
 // config/database.js
 module.exports = {
-    'url' : 'mongodb://SoftographAdmin:softograph1234@ds137650.mlab.com:37650/life-navigator' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
--- controller/api.js
+++ controller/api.js
@@ -3,6 +3,10 @@ let Instance = require('../models/instance'),
     Option = require('../models/option'),
     Home = require('../models/home'),
     Accomplishment = require('../models/accomplishment'),
+    AssessRisk = require('../models/assessRisk'),
+    Industry = require('../models/industry'),
+    Occupation = require('../models/occupation'),
+    Sick = require('../models/sick'),
     FutureGoal = require('../models/futureGoal');
 
 let getCategories = (req, res, next) => {
@@ -22,12 +26,42 @@ let getCategories = (req, res, next) => {
                                 if (err) {
                                     console.log('Error: ' + err);
                                 } else {
-                                    res.status(201).json({
-                                        title: 'My Title',
-                                        futureGoal: futureGoal,
-                                        accomplishment: accomplishment,
-                                        categories: categories,
-                                        instanceId: instance._id
+                                    AssessRisk.findOne({instanceId: instance._id}, (err, assessRisk) => {
+                                        if (err) {
+                                            console.log('Error: ' + err);
+                                        } else {
+                                            getIndustries(instance._id, (err, industries) => {
+                                                if (err) {
+                                                    console.log(err);
+                                                } else {
+                                                    getOccupations(instance._id, (err, occupations) => {
+                                                       if (err) {
+                                                           console.log('Error: ' + err);
+                                                       } else {
+                                                           getSicks(instance._id, (err, sicks) => {
+                                                               if (err) {
+                                                                   console.log('Error: ' + err);
+                                                               } else {
+                                                                   console.log('Get sicks');
+                                                                   console.log(sicks);
+                                                                   res.status(201).json({
+                                                                       title: 'My Title',
+                                                                       futureGoal: futureGoal,
+                                                                       accomplishment: accomplishment,
+                                                                       assessRisk: assessRisk,
+                                                                       categories: categories,
+                                                                       occupations: occupations,
+                                                                       industries: industries,
+                                                                       sicks: sicks,
+                                                                       instanceId: instance._id
+                                                                   });
+                                                               }
+                                                           });
+                                                       }
+                                                    });
+                                                }
+                                            });
+                                        }
                                     });
                                 }
                             });
@@ -53,11 +87,18 @@ let traverseAllCategories = (categories, cb) => {
                 if (err) {
                     return cb (err, null);
                 } else {
-                    categories[index].options = options;
-                    console.log(categories[index].options);
-                    if (categories.length - 1 == index) {
-                        return cb (null, categories);
-                    }
+                    setOptionId(options, (err, options) => {
+                        if (err) {
+                            return cb (err, null);
+                        } else {
+                            categories[index].options = options;
+                            console.log(categories[index].options);
+                            console.log('Length: ' + categories.length + ' index: ' +  index);
+                            if (categories.length - 1 == index) {
+                                return cb (null, categories);
+                            }
+                        }
+                    });
                 }
             });
         }
@@ -78,4 +119,53 @@ let cookCategories = (instanceId, cb) => {
             });
         }
     });
+};
+
+let getIndustries = (instanceId, cb) => {
+    Industry.find({instanceId: instanceId}, (err, industries) => {
+        if (err) {
+            console.log('Error: ' + err);
+            return cb (err, null);
+        } else {
+            return cb (null, industries);
+        }
+    });
+};
+
+let getOccupations = (instanceId, cb) => {
+    Occupation.find({instanceId: instanceId}, (err, occupations) => {
+        if (err) {
+            console.log('Error: ' + err);
+            return cb (err, null);
+        } else {
+            return cb (null, occupations);
+        }
+    });
+};
+
+let getSicks = (instanceId, cb) => {
+    Sick.find({instanceId: instanceId}, (err, sicks) => {
+        if (err) {
+            console.log('Error: ' + err);
+            return cb (err, null);
+        } else {
+            return cb (null, sicks);
+        }
+    });
+};
+
+let setOptionId = (options, cb) => {
+    console.log('Get options');
+    if (options.length == 0) {
+        return cb (null, options);
+    } else {
+        for (let index=0; index<options.length; index++) {
+            options[index].optId = index;
+            console.log('Index: ' + index + ' length: ' + (options.length - 1));
+            if (index == options.length - 1) {
+                // console.log(options);
+                return cb (null, options);
+            }
+        }
+    }
 };
